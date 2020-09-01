2e8acb70966e926a7d0bd1fae39fa676c1bac9e0
liliwang1 <monsi0510@hotmail.com>
2020-09-01 12:36:28 -0500

    cleanup and add comment

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 "use strict"
 
+//============ COFFEES ARRAY ===============//
+
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
@@ -16,23 +18,28 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
+
+//============ DISPLAY FUNCTION ===============//
+
 // Grabs a specific array and sets it up as a string
-var displayCoffee = function(array){
+var displayCoffee = function (array) {
     var html = "";
-    array.forEach(function(coffee){
+    array.forEach(function (coffee) {
         var eachCoffee = `<a href ='#' onclick='storeCoffee("${coffee.name + ": " + coffee.roast}")' class='m-4 p-3'><h2 class='coffee-name text-center'>${coffee.name}</h2><p class='coffee-roast text-center text-uppercase'>${coffee.roast}</p></a>`;
         html += eachCoffee;
     })
     return html;
 }
+
+//============ USER SELECTION AND SEARCH FUNCTION ===============//
+
 // Updates the page with the current array with default 'all'
 updateCoffees();
-
 // select filter functionality
 function updateCoffees() {
     var selectedRoast = document.getElementById('roast-selection').value;
     var filteredCoffees = [];
-    if(selectedRoast === 'all'){
+    if (selectedRoast === 'all') {
         filteredCoffees = coffees;
     }
     coffees.forEach(function (coffee) {
@@ -42,26 +49,29 @@ function updateCoffees() {
     });
 // At this point we have a new array that is filtered from the select
     //show the new array using the displayCoffee(array)
-
     document.getElementById('coffees').innerHTML = displayCoffee(filteredCoffees);
 
     //search by coffee name in the input and uses the filter method to return an array that includes the character using the input onkeyup event listener
-    var filterCoffee = function(){
-        var selectedCoffee = filteredCoffees.filter(function(coffee){
+    var filterCoffee = function () {
+        var selectedCoffee = filteredCoffees.filter(function (coffee) {
             return coffee.name.toLowerCase().includes(document.getElementById('coffee-search').value.toLowerCase());
         })
         document.getElementById('coffees').innerHTML = displayCoffee(selectedCoffee);
     }
     document.getElementById('coffee-search').addEventListener('keyup', filterCoffee);
 }
+
+//============ USER INPUT NEW COFFEE FUNCTION ===============//
+
 //takes user input with select and input values
 //sends alert if empty input is submitted
 //new coffee is pushed on to the 'original' array' applying an object including all the properties
 //if the coffee name and roast are duplicated it will send an alert
 //the new coffee array is re-displayed using the displayCoffee function
+
 function addNewCoffee() {
-    var roastSelect= document.getElementById('roast-addition');
-    var textAddition= document.getElementById("new-name");
+    var roastSelect = document.getElementById('roast-addition');
+    var textAddition = document.getElementById("new-name");
 
     if (textAddition.value === '') {
         alert('Please put in a valid coffee name');
@@ -77,64 +87,68 @@ function addNewCoffee() {
         document.getElementById('coffees').innerHTML = displayCoffee(coffees);
         storeIntoLocal(newCoffee);
     }
-
 }
 //submit button listener event
 document.getElementById('new-button').addEventListener('click', addNewCoffee);
 
+//============ SHOPPING CART FUNCTION ===============//
+
 //Refactored function that has the shopping cart items html structure
 
-function shoppingPartElement (dataShow, i){
+function shoppingPartElement(dataShow, i) {
     return "<p class='cart-row' id = '" + i + "'><span class='shopping-name mr-5'>" + dataShow + "</span><a class='ml-auto cart-logo' href='#' onclick = deleteItem(" + i + ")>Remove</a></p><hr>";
 }
-function removeChild(){let parent = document.getElementById('cart_box');
-    while (parent.firstChild){
+
+function removeChild() {
+    let parent = document.getElementById('cart_box');
+    while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
+
 //TODO: Set up the default display. A separate array for the data and shopping cart.
 
 let shoppingArray = [];
-function defaultShopping(){
+function defaultShopping() {
     removeChild();
-   for(var i = 0; i < 20; i++) {
-    if (window.localStorage.getItem('coffee' + i)) {
-        let dataShow = window.localStorage.getItem('coffee' + i);
-        let divResponse = document.createElement('div');
-        divResponse.innerHTML = shoppingPartElement(dataShow, i);
-        document.getElementById('cart_box').appendChild(divResponse);
-        shoppingArray.push(dataShow);
-    }
-
+    for (var i = 0; i < 20; i++) {
+        if (window.localStorage.getItem('coffee' + i)) {
+            let dataShow = window.localStorage.getItem('coffee' + i);
+            let divResponse = document.createElement('div');
+            divResponse.innerHTML = shoppingPartElement(dataShow, i);
+            document.getElementById('cart_box').appendChild(divResponse);
+            shoppingArray.push(dataShow);
+        }
     }
 }
 defaultShopping();
 
-
 //TODO: onclick function that stores the data by adding to the 'current' array first and then storing it. Every time onclick is activated the local memory is stored again.
-let storeCoffee = function(input){
+
+let storeCoffee = function (input) {
     shoppingArray.push(input); //updates the shopping cart array
-    shoppingArray.forEach(function (shopping_item, i){
+    shoppingArray.forEach(function (shopping_item, i) {
         window.localStorage.setItem('coffee' + i, shopping_item);
     }); //updates the data by saving over it
-   removeChild(); //removes previous html elements
-    for (var i = 0; i < shoppingArray.length; i++){
+    removeChild(); //removes previous html elements
+    for (var i = 0; i < shoppingArray.length; i++) {
         let dataShow = window.localStorage.getItem('coffee' + i);
         let divResponse = document.createElement('div');
         divResponse.innerHTML = shoppingPartElement(dataShow, i);
         document.getElementById('cart_box').appendChild(divResponse);
-
     }
-
 }
+
 //TODO: Get rid of any div in parent container before new display. New display is going to create divs based on data. Each key will have its own div.
-let clearLocal = function() {
+
+let clearLocal = function () {
     let dataClear = window.localStorage.clear();
     shoppingArray = [];
     document.getElementById('cart_box').innerHTML = "";
-
 }
+
 //TODO: add onclick on "remove" to delete from memory and display that specific div
+
 let deleteItem = function (input) {
     removeChild(); //removing the previous HTML element
     window.localStorage.removeItem('coffee' + input);
@@ -146,10 +160,13 @@ let deleteItem = function (input) {
             divResponse.innerHTML = shoppingPartElement(dataShow, i);
             document.getElementById('cart_box').appendChild(divResponse);
         }
-
     }
 }
+
+//============ SAVING TO LOCALSTORAGE FUNCTION ===============//
+
 // A much better way to store the data for future applications due to its ability to store objects. Objects have more accessibility when trying to use various properties.
+
 function storeIntoLocal(newItem) {
     var coffeeJSON = JSON.stringify(newItem);
     localStorage.setItem('newCoffee' + newItem.id, coffeeJSON);
@@ -164,4 +181,3 @@ function storeIntoLocal(newItem) {
 //         i++;
 //     }
 // }
-// console.log(coffees);
